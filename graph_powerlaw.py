c9ab5bc5a935a09eccd96fd2a2eae67dfedcd105
fsossai <federico.sossai@gmail.com>
2023-04-23 21:51:39 -0500

    Added random attachment and Erdos-Renyi

--- graph_powerlaw.py
+++ graph_powerlaw.py
@@ -10,12 +10,32 @@ G.add_node(0)
 G.add_node(1)
 G.add_edge(0, 1)
 
-for i in range(2, N):
-    degs = numpy.array([d for _, d in G.degree()])
-    cdf = numpy.cumsum(degs)
-    n = numpy.argmax(cdf > random.randint(0, (i-1)*2-1))
-    G.add_edge(i, n)
-    
+def preferential_attachment(m=1):
+    for i in range(2, N):
+        degs = numpy.array([d for _, d in G.degree()])
+        cdf = numpy.cumsum(degs)
+        for _ in range(m):
+            n = numpy.argmax(cdf > random.randint(0, (i-1)*2-1))
+            G.add_edge(i, n)
+
+def random_attachment(m=1):
+    for i in range(2, N):
+        for _ in range(m):
+            n = numpy.random.randint(0, i-1)
+            G.add_edge(i, n)
+
+def erdos_renyi(m=1):
+    p = m * (2/(N-1))
+    G.add_nodes_from(range(N))
+    for i in range(N):
+        for j in range(i+1, N):
+            if numpy.random.rand() < p:
+                G.add_edge(i, j)
+
+#erdos_renyi(1)
+#random_attachment(1)
+preferential_attachment(1)
+
 N = len(G.nodes)
 M = len(G.edges)
 

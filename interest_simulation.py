66a68583ca88a72aa9e5e5a767cd1655c6a76411
fsossai <federico.sossai@gmail.com>
2024-01-02 12:48:16 -0600

    Fixed `interest_simulation.py`

--- interest_simulation.py
+++ interest_simulation.py
@@ -1,10 +1,10 @@
 import numpy
 import matplotlib.pyplot as plt
 
-init = 0 # initial amount [$]
-spm = 5000 # savings per month [$]
-ir = 10 # interest rate [%]
-years = 30 # number of years to simulate
+init = 1200 # initial amount [$]
+spm = 25 # savings per month [$]
+ir = 4.50 # interest rate [%]
+years = 5 # number of years to simulate
 
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -29,14 +29,14 @@ print(f"Savings        : ${savings:13.2f}")
 print(f"Interests      : ${interests:13.2f}")
 print(f"Final interest : +{ratio*100:.1f}%")
 
-f = lambda y, r: spm * (numpy.exp(y*r)-1) / (numpy.exp(r/12)-1)
+f = lambda y, r: spm*(numpy.exp(y*r)-1) / (numpy.exp(r/12)-1)
 x = numpy.arange(1, years+1)
-plt.plot(x, x * spm * 12, "--", label="R=0%")
-plt.plot(x, f(x, 0.03), label="R=3%")
-plt.plot(x, f(x, 0.05), label="R=5%")
-plt.plot(x, f(x, 0.07), label="R=7%")
-plt.plot(x, f(x, 0.10), label="R=10%")
-plt.axhline(1e6, 0, 1, linestyle="--", color="red")
+plt.plot(x, init + x * spm * 12, "--", label="R=0%")
+plt.plot(x, init+f(x, 0.03), label="R=3%")
+plt.plot(x, init+f(x, 0.05), label="R=5%")
+plt.plot(x, init+f(x, 0.07), label="R=7%")
+plt.plot(x, init+f(x, 0.10), label="R=10%")
+#plt.axhline(1e6, 0, 1, linestyle="--", color="red")
 plt.xlabel("Time [years]")
 plt.ylabel("Savings [$]")
 plt.legend()

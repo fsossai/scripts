5fa25c6736db26d62a04e9acf345af0dcf68bb31
fsossai <federico.sossai@gmail.com>
2024-11-27 15:21:17 -0600

    qplot: supporting self speedup

--- qplot.py
+++ qplot.py
@@ -10,7 +10,8 @@ import sys
 
 parser = argparse.ArgumentParser(
     description="Generating speedup plots from raw time measurements in CSV format.\n"
-                "The CSV input files must contain a column named 'threads' and one named 'time' (in ms).\n")
+                "The CSV input files must contain a column named 'threads' and one "
+                "named 'time' (in ms).\n")
 
 parser.add_argument("filenames", metavar="FILENAMES", type=str, nargs="+",
     help="One or more CSV files containing the columns 'threads' and 'time' (in ms)")
@@ -18,13 +19,16 @@ parser.add_argument("filenames", metavar="FILENAMES", type=str, nargs="+",
 parser.add_argument("-u", "--unit", type=str, choices=["s", "ms"], default="s", 
     help="Time unit. Default is 's' (seconds)")
 
-parser.add_argument("-b", "--baseline", metavar="FILENAME", type=str, default=None, required=True,
+parser.add_argument("-b", "--baseline", metavar="FILENAME", type=str, default=None,
     help="The file from which to extract the baseline used for the calculation of the speedup."
-         "In case there are runs with different numbers of threads, only thread==1 runs are considered")
+         "In case there are runs with different numbers of threads, only thread==1 "
+         "runs are considered")
 
-parser.add_argument("-c", "--confidence-interval", type=str, choices=["std", "mm", "mad"], default="std",
+parser.add_argument("-c", "--confidence-interval", type=str, choices=["std", "mm", "mad"],
+    default="std",
     help="Policy for the bounds of the intervals. 'std', 'mm', 'mad' stand for"
-         "'standard deviation', 'minmax', 'meadian absolute deviation' respectively. Default is 'std'")
+         "'standard deviation', 'minmax', 'meadian absolute deviation' respectively. "
+         "Default is 'std'")
 
 parser.add_argument("-s", "--n-sigmas", type=int, choices=[0, 1, 2, 3], default=3,
     help="Confidence intervals expressed in multiples of the standard deviation. "
@@ -43,7 +47,8 @@ parser.add_argument("--hide-time-plot", default=False, action="store_true",
     help="Hide the absolute time plot")
 
 parser.add_argument("--hide-peaks", action="store_true",
-    help="Hide the horizontal and vertical lines related to maximum speedup and minimum execution time")
+    help="Hide the horizontal and vertical lines related to maximum speed "
+          "and minimum execution time")
 
 parser.add_argument("--amdahl", action="store_true",
     help="Attempt to fit Amdahl's law to the speedup plot")
@@ -52,8 +57,8 @@ parser.add_argument("--boundaries", action="store_true", default=False,
     help="Draw min-max boundaries around each line")
 
 parser.add_argument("--attitude", type=str, choices=["fair", "pessimistic"], default="pessimistic",
-    help="'fair' and 'pessimistic' use 'median' and 'min' for computing a baseline from multiple runs. "
-         "Default is 'pessimistic'")
+    help="'fair' and 'pessimistic' use 'median' and 'min' for computing a baseline from multiple "
+         "runs. Default is 'pessimistic'")
 
 parser.add_argument("-n", "--names", type=str, 
     help="Rename programs in the plot legend. ;-separated list")
@@ -64,7 +69,8 @@ parser.add_argument("-t", "--title", type=str, default="",
 parser.add_argument("-o", "--output", metavar="FILENAME", type=str,
     help="Specify a file where to dump the plot")
 
-preferred_colors = ["#5588dd", "#882255", "#33bb88", "#ddcc77", "#cc6677", "#999933", "#aa44ff", "#448811"]
+preferred_colors = ["#5588dd", "#882255", "#33bb88", "#ddcc77",
+                    "#cc6677", "#999933", "#aa44ff", "#448811"]
 preferred_color = iter(preferred_colors)
 name_sep = "::"
 
@@ -94,55 +100,58 @@ def make_line_ci(axes, x, y, low, high, alpha):
     for x_val, y_val, l, h in zip(x, y, low, high):
         axes.vlines(x=x_val, ymin=l, ymax=h, color=color, alpha=alpha, linewidth=4)
 
-# handling baseline
-df = pandas.read_csv(args.baseline)
-df = df.dropna()
-if args.unit == "s":
-    df["time"] /= 1000
-
-color = preferred_colors[len(args.filenames)]
-baseline_times = df.groupby("threads")["time"].median()
-
-if args.attitude == "fair":
-    baseline_time = df.groupby("threads")["time"].median()[1]
-elif args.attitude == "pessimistic":
-    baseline_time = df.groupby("threads")["time"].min()[1]
-
-baseline_std = df.groupby("threads")["time"].std()
-baseline_std = baseline_std.fillna(0.0)
-baseline_mins = df.groupby("threads")["time"].min()
-baseline_maxs = df.groupby("threads")["time"].max()
 f_mad = lambda x : (x - x.median()).abs().median()
-baseline_mad = df.groupby("threads")["time"].apply(f_mad)
 
-def upper(sigma_coeff):
-    if args.confidence_interval == "std":
-        return [baseline_time + sigma_coeff * baseline_std[1]]
-    elif args.confidence_interval == "mm":
-        return [baseline_maxs[1]]
-    elif args.confidence_interval == "mad":
-        return [baseline_time + baseline_mad[1]]
+# handling baseline
+if args.baseline is not None:
+    df = pandas.read_csv(args.baseline)
+    df = df.dropna()
+    if args.unit == "s":
+        df["time"] /= 1000
 
-def lower(sigma_coeff):
-    if args.confidence_interval == "std":
-        return [baseline_time - sigma_coeff * baseline_std[1]]
-    elif args.confidence_interval == "mm":
-        return [baseline_mins[1]]
-    elif args.confidence_interval == "mad":
-        return [baseline_time - baseline_mad[1]]
+    color = preferred_colors[len(args.filenames)]
+    baseline_times = df.groupby("threads")["time"].median()
 
-# confidence intervals for the baseline (time plot)
-if args.n_sigmas >= 1:
-    make_line_ci(axs[1], [1], [baseline_time], lower(1), upper(1), alpha=0.30)
-if args.n_sigmas >= 2:
-    make_line_ci(axs[1], [1], [baseline_time], lower(2), upper(2), alpha=0.20)
-if args.n_sigmas >= 3:
-    make_line_ci(axs[1], [1], [baseline_time], lower(3), upper(3), alpha=0.10)
+    if args.attitude == "fair":
+        baseline_time = df.groupby("threads")["time"].median()[1]
+    elif args.attitude == "pessimistic":
+        baseline_time = df.groupby("threads")["time"].min()[1]
+
+    baseline_std = df.groupby("threads")["time"].std()
+    baseline_std = baseline_std.fillna(0.0)
+    baseline_mins = df.groupby("threads")["time"].min()
+    baseline_maxs = df.groupby("threads")["time"].max()
+    baseline_mad = df.groupby("threads")["time"].apply(f_mad)
+
+    def upper(sigma_coeff):
+        if args.confidence_interval == "std":
+            return [baseline_time + sigma_coeff * baseline_std[1]]
+        elif args.confidence_interval == "mm":
+            return [baseline_maxs[1]]
+        elif args.confidence_interval == "mad":
+            return [baseline_time + baseline_mad[1]]
+
+    def lower(sigma_coeff):
+        if args.confidence_interval == "std":
+            return [baseline_time - sigma_coeff * baseline_std[1]]
+        elif args.confidence_interval == "mm":
+            return [baseline_mins[1]]
+        elif args.confidence_interval == "mad":
+            return [baseline_time - baseline_mad[1]]
+
+    # confidence intervals for the baseline (time plot)
+    if args.n_sigmas >= 1:
+        make_line_ci(axs[1], [1], [baseline_time], lower(1), upper(1), alpha=0.30)
+    if args.n_sigmas >= 2:
+        make_line_ci(axs[1], [1], [baseline_time], lower(2), upper(2), alpha=0.20)
+    if args.n_sigmas >= 3:
+        make_line_ci(axs[1], [1], [baseline_time], lower(3), upper(3), alpha=0.10)
 
 max_x = 1
 
 names = []
 dfs = []
+min_thread_num = None
 
 for filename in args.filenames:
     name = filename.rsplit(".", 1)[0].split("/")[-1] if args.names is None else next(name_it)
@@ -167,9 +176,31 @@ for name, df in zip(names, dfs):
 
     # ===== SPEEDUP PLOT ================================================================
 
+    if args.baseline is None:
+        n = df["threads"].min()
+        min_thread_num = min(min_thread_num or 2**30, n)
+        if min_thread_num != n:
+            print("The minumum number of threads in each file should be the same "
+                  "when no explicit baseline is specified.\n"
+                  f"Found two experiments with min thread num equal to {min_thread_num} "
+                  f"and {n} respectively.")
+            sys.exit(1)
+
+        if args.attitude == "fair":
+            baseline_time = df.groupby("threads")["time"].median()[n]
+        elif args.attitude == "pessimistic":
+            baseline_time = df.groupby("threads")["time"].min()[n]
+
     speedup = baseline_time / median
     x = median.index.to_numpy(dtype=int)
     time = median.values
+
+    if args.baseline is None:
+        # for the case of self speedup (i.e. no explicit baseline) it would be 
+        # counterintuitive to not have the speedup plot starting at 1 and the time
+        # plot starting from the baseline time
+        speedup[min_thread_num] = 1.0
+        time[0] = baseline_time
 
     def upper(sigma_coeff):
         if args.confidence_interval == "std":
@@ -189,8 +220,9 @@ for name, df in zip(names, dfs):
 
     label = "{} {} max={:.1f}x @ T={}".format(name, name_sep, max(speedup), speedup.idxmax())
 
-    if min(speedup) < 1.0:
-        axs[0].axhline(y=1.0, linestyle="-", linewidth=1, color="#00ff00")
+    if args.baseline is not None:
+        if min(speedup) < 1.0:
+            axs[0].axhline(y=1.0, linestyle="-", linewidth=1, color="#00ff00")
 
     color = next(preferred_color)
     axs[0].plot(x, speedup, ".-", label=label, color=color)
@@ -225,7 +257,8 @@ for name, df in zip(names, dfs):
 
     # ===== TIME PLOT ===================================================================
 
-    label = "{} {} min={:.1f} {} @ T={}".format(name, name_sep, min(time), args.unit, median.index[time.argmin()])
+    label = "{} {} min={:.1f} {} @ T={}".format(
+            name, name_sep, min(time), args.unit, median.index[time.argmin()])
     axs[1].plot(x, time, ".-", label=label, color=color)
 
     def upper(sigma_coeff):
@@ -268,13 +301,16 @@ for name, df in zip(names, dfs):
     # printing a brief summary to the terminal
     longest_name = max([len(n) for n in names])
     padding = longest_name + 1
-    print("{:{padding}}: max speedup = {:.1f}x ({:.1f} {}) @ T={}".format(name, max(speedup), min(time), args.unit, speedup.idxmax(), padding=padding))
+    print("{:{padding}}: max speedup = {:.1f}x ({:.1f} {}) @ T={}"
+          .format(name, max(speedup), min(time), args.unit, speedup.idxmax(), padding=padding))
 
     max_x = max(max_x, df["threads"].max())
 
 color = next(preferred_color)
-axs[1].plot(1, baseline_time, marker="*", linestyle="None", markersize=10, color=color, label="Baseline time = {:.1f} {}".format(baseline_time, args.unit))
-print("Reference time = {:.1f} {}".format(baseline_time, args.unit))
+if args.baseline is not None:
+    axs[1].plot(1, baseline_time, marker="*", linestyle="None", markersize=10, color=color,
+                label="Baseline time = {:.1f} {}".format(baseline_time, args.unit))
+    print("Reference time = {:.1f} {}".format(baseline_time, args.unit))
 
 # print ideal linear speedup
 xmax = axs[0].get_xlim()[1]
@@ -288,14 +324,20 @@ axs[0].set_xlabel("Number of threads (T)")
 axs[0].set_ylabel("Speedup")
 axs[0].legend()
 axs[0].set_ylim(bottom=0.0)
-axs[0].set_xlim(left=0)
+
+if args.baseline is None:
+    axs[0].set_xlim(left=min_thread_num)
+    axs[1].set_xlim(left=min_thread_num)
+else:
+    axs[0].set_xlim(left=0)
+    axs[1].set_xlim(left=0)
+
 axs[0].grid(True)
 axs[1].set_xticks(x_range, x_range, rotation="vertical")
 axs[1].set_xlabel("Number of threads (T)")
 axs[1].set_ylabel("Execution time [{}]".format(args.unit))
 axs[1].legend()
 axs[1].set_ylim(bottom=0.0)
-axs[1].set_xlim(left=0)
 axs[1].grid(True)
 fig.suptitle(args.title)
 

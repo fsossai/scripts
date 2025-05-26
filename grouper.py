9a43a1fee57946a18f869b7259ff98ddb7b7be29
fsossai <federico.sossai@gmail.com>
2025-05-26 14:14:34 -0500

    grouper: rsync

--- grouper.py
+++ grouper.py
@@ -4,45 +4,70 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import scipy.stats
+import subprocess
 import threading
 import argparse
+import datetime
 import pathlib
 import hashlib
 import spread
 import time
+import sys
+
+def validate_files():
+    valid_files = []
+    valid_formats = [".json", ".csv"]
+    for file in args.files:
+        if pathlib.Path(file).suffix in valid_formats:
+            valid_files.append(file)
+        else:
+            print(f"Unsupported file format {file}")
+    return valid_files
+
+def get_local_mirror(rfile):
+    return pathlib.Path(rfile.split(":")[1]).name
+
+def locate_files():
+    local_files = []
+    for file in valid_files:
+        if is_remote(file):
+            local_files.append(get_local_mirror(file))
+        else:
+            local_files.append(file)
+    return local_files
 
 def generate_input_dataframe():
     global args
     dfs = dict()
-    for file in args.files:
+    for file in local_files:
         file = pathlib.Path(file)
         if file.suffix == ".json":
             dfs[file.stem] = pd.read_json(file, lines=True)
         elif file.suffix == ".csv":
             dfs[file.stem] = pd.read_csv(file)
-        else:
-            print(f"Unsupported file format {file}")
     df = pd.concat(dfs)
     df.index.names = ["file", None]
     df = df.reset_index(level=0, drop=(len(dfs) == 1))
     df = df.reset_index(level=0, drop=True)
     return df
 
-def monitor(files):
+def monitor():
     global df
     current_hash = None
     last_hash = None
 
-    while plt.fignum_exists(fig.number):
+    while alive:
         try:
             current_hash = ""
-            for file in files:
+            for file in local_files:
                 with open(file, "rb") as f:
                     current_hash += hashlib.md5(f.read()).hexdigest()
         except FileNotFoundError:
             current_hash = None
         if current_hash != last_hash:
             df = generate_input_dataframe()
+            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{now}: data updated")
             update_table()
             update_plot()
         last_hash = current_hash
@@ -62,6 +87,32 @@ def update_table():
     ax_table.table(cellText=[text], colLabels=labels,
                    cellLoc="center", edges="open", loc="center")
     fig.canvas.draw_idle()
+
+def is_remote(file):
+    return "@" in file
+
+def sync_files(interval):
+    jobs = []
+    for file in valid_files:
+        if is_remote(file):
+            mirror = get_local_mirror(file)
+            proc = subprocess.run(["scp", file, mirror])
+            if proc.returncode != 0:
+                print(f"scp transfer failed for {file}")
+                sys.exit(1)
+            jobs.append((file, mirror))
+
+    def rsync(src, dst):
+        while alive:
+            subprocess.run(
+                ["rsync", "-z", "--checksum", src, dst],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL
+            )
+            time.sleep(interval)
+
+    for job in jobs:
+        threading.Thread(target=rsync, daemon=True, args=job).start()
 
 def update_plot(direction="none"):
     x = {"left": -1, "right": 1, "none": 0}
@@ -106,6 +157,10 @@ def on_key(event):
         selected_dim = dims[int(event.key) - 1]
         update_table()
 
+def on_close(event):
+    global alive
+    alive = False
+
 parser = argparse.ArgumentParser()
 parser.add_argument("files", metavar="FILES", type=str, nargs="+", help="JSON Lines or CSV files")
 parser.add_argument("-x", required=True, help="X-axis column name")
@@ -113,7 +168,12 @@ parser.add_argument("-y", required=True, help="Y-axis column name")
 parser.add_argument("-z", required=False, default=None, help="Grouping column name")
 parser.add_argument("-b", "--baseline", default=None, help="Baseline group value in -z to normalize y-axis")
 parser.add_argument("-m", "--spread-measure", default="mad", help="Measure of dispersion. Available: " + ", ".join(spread.available))
+parser.add_argument("-r", "--rsync-interval", type=float, default=5, help="[seconds] Remote synchronization interval")
 args = parser.parse_args()
+alive = True
+valid_files = validate_files()
+local_files = locate_files()
+sync_files(args.rsync_interval)
 
 df = generate_input_dataframe()
 
@@ -131,14 +191,11 @@ fig, axs = plt.subplots(2, 1, gridspec_kw={"height_ratios": [1, 10]})
 fig.set_size_inches(10, 8)
 ax_table = axs[0]
 ax_plot = axs[1]
-
 sns.set_theme(style="whitegrid")
 ax_plot.grid(axis="y")
-
-
 fig.canvas.mpl_connect("key_press_event", on_key)
+fig.canvas.mpl_connect("close_event", on_close)
 plt.tight_layout()
-
-threading.Thread(target=monitor, daemon=True, args=(args.files,)).start()
+threading.Thread(target=monitor, daemon=True).start()
 plt.show()
 
